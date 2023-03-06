73fc65fdfcaf29fc2ec54e1a8df6cdd2793f95c5
dyn4casie <dyn4casie@gmail.com>
2023-03-06 14:45:10 +0100

    Faqs and Testimonials completed

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
 })
+
+//show/hidw faq answers
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon when opened
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = "uil uil-plus";
+        }
+    })
+})
